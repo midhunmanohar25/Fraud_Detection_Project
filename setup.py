7507784202407b23a5520fb227a60577418d85d3
Midhun Manohar <midhunmanohar25@gmail.com>
2026-02-17 18:17:10 +0530

    Updated the Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Built a machine learning–based fraud detection system to identify suspicious transactions and reduce financial risk.',
+    author='Midhun Manohar',
+    license='',
+)
